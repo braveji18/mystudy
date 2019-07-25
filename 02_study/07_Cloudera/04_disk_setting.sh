ab93393501b7f64e61da744feb8f834db41f8360
YONG JI <braveji18@gmail.com>
2019-07-25 14:10:46 +0900

    aa

--- 02_study/07_Cloudera/04_disk_setting.sh
+++ 02_study/07_Cloudera/04_disk_setting.sh
@@ -11,13 +11,17 @@
 DISK_DEV="/dev/sdb"
 DISK_DEV="/dev/sdc"
 
-fdisk ${DISK_DEV}
-mkfs.ext4 -m 0 ${DISK_DEV}1
+echo -e "n\np\n1\n\n\nw" | fdisk  ${DISK_DEV}
+
+#mkfs.ext4 -m 0 ${DISK_DEV}1
+mkfs.xfs       ${DISK_DEV}1
 
 mkdir -p /home/data01
-mount -t ext4 -o noatime ${DISK_DEV}1 /home/data01
 
-echo "${DISK_DEV}1   /home/data01    ext4    defaults,noatime     0 0"   >> /etc/fstab
+#mount -t ext4 -o noatime ${DISK_DEV}1 /home/data01
+mount -t xfs  ${DISK_DEV}1 /home/data01
+
+#echo "${DISK_DEV}1   /home/data01    ext4    defaults,noatime     0 0"   >> /etc/fstab
+echo "${DISK_DEV}1   /home/data01    xfs     defaults,noatime     0 0"   >> /etc/fstab
 
 cat /etc/fstab 
-
